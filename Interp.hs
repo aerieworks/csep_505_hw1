a64c956d4666ba60e9d45ee22b6d607b87921633
Annabelle Richard <annabelle.e.richard@gmail.com>
2015-01-17 17:36:35 -0800

    Implemented type checking in parseExpr.

--- Interp.hs
+++ Interp.hs
@@ -23,17 +23,22 @@ data Expr = NumE Integer
 parseExpr :: SExp -> Result Expr
 parseExpr sexp =
   case sexp of
-    NumS n                             -> Ok (NumE n)
-    IdS "true"                         -> Ok (BoolE True)
-    IdS "false"                        -> Ok (BoolE False)
-    ListS ((IdS "if"):cond:true:false:[]) -> parseIfE (parseExpr cond) (parseExpr true) (parseExpr false)
-    ListS ((IdS op):x:y:[])               -> parseBinE (parseBinOp op) (parseExpr x) (parseExpr y)
+    NumS n                                -> Ok (NumE n)
+    IdS "true"                            -> Ok (BoolE True)
+    IdS "false"                           -> Ok (BoolE False)
+    ListS ((IdS "if"):cond:true:false:[]) -> parseIfE cond true false
+    ListS ((IdS op):x:y:[])               -> parseBinE op x y
+    otherwise                             -> Err "Invalid syntax."
 
-parseBinE :: Result BinOp -> Result Expr -> Result Expr -> Result Expr
-parseBinE (Err str) _ _ = Err str
-parseBinE _ (Err str) _ = Err str
-parseBinE _ _ (Err str) = Err str
-parseBinE (Ok op) (Ok x) (Ok y) = Ok (BinOpE op x y)
+parseBinE :: String -> SExp -> SExp -> Result Expr
+parseBinE opStr xSExp ySExp =
+  case parseBinOp opStr of
+    Err str   -> Err str
+    Ok op     -> case parseNumExpr xSExp of
+                  Err str -> Err str
+                  Ok x    -> case parseNumExpr ySExp of
+                               Err str -> Err str
+                               Ok y    -> Ok (BinOpE op x y)
 
 parseBinOp :: String -> Result BinOp
 parseBinOp str =
@@ -44,17 +49,69 @@ parseBinOp str =
     "<" -> Ok Lt
     otherwise -> Err ("Unknown binary operator '" ++ str ++ "'.")
 
-parseIfE :: Result Expr -> Result Expr -> Result Expr -> Result Expr
-parseIfE (Err str) _ _ = Err str
-parseIfE _ (Err str) _ = Err str
-parseIfE _ _ (Err str) = Err str
-parseIfE (Ok x) (Ok y) (Ok z) = Ok (IfE x y z)
+parseNumExpr :: SExp -> Result Expr
+parseNumExpr sexp =
+  let exp = parseExpr sexp
+  in case exp of
+    Ok (BinOpE Add _ _)  -> exp
+    Ok (BinOpE Mult _ _) -> exp
+    Ok (NumE _)          -> exp
+    Ok _                 -> Err "Expected numeric expression."
+    otherwise            -> exp
+
+parseBoolExpr :: SExp -> Result Expr
+parseBoolExpr sexp =
+  let exp = parseExpr sexp
+  in case exp of
+    Ok (IfE _ _ _)        -> exp
+    Ok (BoolE _)          -> exp
+    Ok (BinOpE Equal _ _) -> exp
+    Ok (BinOpE Lt _ _)    -> exp
+    Ok _                  -> Err "Expected boolean expression."
+    otherwise             -> exp
+
+parseIfE :: SExp -> SExp -> SExp -> Result Expr
+parseIfE condSExp trueSExp falseSExp =
+  case parseBoolExpr condSExp of
+    Err str -> Err str
+    Ok cond -> case parseNumExpr trueSExp of
+                 Err str -> Err str
+                 Ok true -> case parseNumExpr falseSExp of
+                               Err str  -> Err str
+                               Ok false -> Ok (IfE cond true false)
 
 validParseExamples = [
   ("non-trivial example", "(if (= (* 2 3) (+ 5 1)) 7 10)",
    IfE (BinOpE Equal (BinOpE Mult (NumE 2) (NumE 3))
-        (BinOpE Add (NumE 5) (NumE 1))) (NumE 7) (NumE 10))
+        (BinOpE Add (NumE 5) (NumE 1))) (NumE 7) (NumE 10)),
+  ("number", "5", NumE 5),
+  ("boolean true", "true", BoolE True),
+  ("boolean false", "false", BoolE False),
+  ("arithmetic", "(* (+ 4 7) (+ 3 5))", BinOpE Mult (BinOpE Add (NumE 4) (NumE 7))
+        (BinOpE Add (NumE 3) (NumE 5))),
+  ("negative numbers", "(+ 5 -4)", BinOpE Add (NumE 5) (NumE (-4))),
+  ("comparison", "(= 1 2)", BinOpE Equal (NumE 1) (NumE 2)),
+  ("if statement", "(if (= (+ 2 4) (* 3 2)) (+ 3 (* 5 2)) 7)", IfE (BinOpE Equal
+    (BinOpE Add (NumE 2) (NumE 4)) (BinOpE Mult (NumE 3) (NumE 2)))
+    (BinOpE Add (NumE 3) (BinOpE Mult (NumE 5) (NumE 2))) (NumE 7))
   -- Feel free to add your own examples ...
+  ]
+
+invalidParseExamples = [
+  ("missing right operand", "(+ 5)"),
+  ("missing left operand", "(+)"),
+  ("missing else clause", "(if (= x y) z)"),
+  ("missing if clause", "(if (= x y))"),
+  ("missing conditional", "(if)"),
+  ("Type mismatch in addition left operand", "(+ (= 1 2) 3)"),
+  ("Type mismatch in addition right operand", "(+ 1 (= 2 3))"),
+  ("Type mismatch in multiplication left operand", "(* (= 1 2) 3)"),
+  ("Type mismatch in multiplication right operand", "(* 1 (= 2 3))"),
+  ("Type mismatch in equals left operand", "(= (= 1 2) 3)"),
+  ("Type mismatch in equals right operand", "(= 1 (= 2 3))"),
+  ("Type mismatch in less than left operand", "(< (= 1 2) 3)"),
+  ("Type mismatch in less than right operand", "(< 1 (= 2 3))"),
+  ("Type mismatch in conditional", "(if (+ 5 3) 1 2)")
   ]
 
 checkValidParseExample (description, str, expected) =
@@ -62,6 +119,19 @@ checkValidParseExample (description, str, expected) =
    case parseSExp (tokenize str) of
     Ok (sexp, []) -> parseExpr sexp == Ok expected
     _ -> False)
+
+checkInvalidParseExample (description, input) =
+  case parseSExp (tokenize input) of
+    Err str      -> (description, False, str)
+    Ok (sexp, _) -> case parseExpr sexp of
+                      Err str -> (description, True, str)
+                      Ok exp  -> (description, False, show exp)
+
+parse :: String -> Result Expr
+parse input =
+  case parseSExp (tokenize input) of
+    Ok (sexp, _) -> parseExpr sexp
+    Err str      -> Err str
 
 interp :: Expr -> Result Expr
 interp expr = Err "unimplemented"
